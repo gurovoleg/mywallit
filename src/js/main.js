1b020b24f972b6fd29052f9c9547e4a556eae810
maryka <marykaweb@gmail.com>
2018-08-26 23:33:07 +0300

    added header search

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,37 @@ $(document).ready(function() {
 		$('.container').toggleClass('container--left');
 	});
 
+	/* Show search result */
+	$('input.header-search__input').keydown(function(){
+		var inputVal = $(this).val().length;
+		keycode = window.event.keyCode;
+		console.log(keycode);
+
+		if( $(document).width() <= 784 ) {
+			$('.header-search-result-mobile').slideDown(400);
+			if( keycode == 8 && inputVal == 1 ) {
+				$('.header-search-result-mobile').slideUp(400);
+			}
+		}
+
+		if( $(document).width() > 784 ) {
+			$('.header-search-result-desktop').slideDown(400).css("display", "flex");
+			if( keycode == 8 && inputVal == 1 ) {
+				$('.header-search-result-desktop').slideUp(400);
+			}
+		}
+
+		$(window).resize(function(){
+			if( $(window).width() <= 784 ) {
+				$('.header-search-result-desktop').css("display", "none");
+				$('.header-search-result-mobile').css("display", "block");
+			}
+			if( $(window).width() > 784 ) {
+				$('.header-search-result-desktop').css("display", "flex");
+				$('.header-search-result-mobile').css("display", "none");
+			}
+		});
+	});
+	/* //Show search result */
+
 });
