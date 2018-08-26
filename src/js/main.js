c9e014faff58523f0b6259c051188aa9e7a3449e
maryka <marykaweb@gmail.com>
2018-08-26 23:52:18 +0300

    fixed resize with empty input

--- src/js/main.js
+++ src/js/main.js
@@ -27,12 +27,24 @@ $(document).ready(function() {
 			}
 		}
 
+		$('.header-search__close').on('click', function(){
+			$('input.header-search__input').val('');
+			if( $(document).width() <= 784 ) {
+				$('.header-search-result-mobile').slideUp(400);
+				$('.header-search-result-desktop').slideUp(400);
+			}
+			if( $(document).width() > 784 ) {
+				$('.header-search-result-mobile').slideUp(400);
+				$('.header-search-result-desktop').slideUp(400);
+			}
+		});
+
 		$(window).resize(function(){
-			if( $(window).width() <= 784 ) {
+			if( $(window).width() <= 784 && $('input.header-search__input').val() != '') {
 				$('.header-search-result-desktop').css("display", "none");
 				$('.header-search-result-mobile').css("display", "block");
 			}
-			if( $(window).width() > 784 ) {
+			if( $(window).width() > 784 && $('input.header-search__input').val() != '') {
 				$('.header-search-result-desktop').css("display", "flex");
 				$('.header-search-result-mobile').css("display", "none");
 			}
