6065340d107e9e8e9414a1a782ac6a6f4ba9e9e6
gurovoleg <gogotheog@gmail.com>
2018-09-01 16:56:43 +0300

    separate script file for city selector

---
+++ src/js/city-selector.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+	(function(){
+
+		var $cityList = $('.city-selection-wrapper'), // обертка
+			$toggle = $('.city-toggle'), // переключатель
+			$close = $('.close-selection'); // выключатель
+
+		// Касмтоный скролл для списка
+		$($cityList).niceScroll({
+				cursorcolor:"#ACACAC",
+				cursorwidth:"8px",
+				background:"#DBDBDB",
+				cursorborder:"none",
+				cursorborderradius:4,
+				cursorfixedheight: 70,
+				scrollspeed: 300,
+				autohidemode: false			
+		}); 	
+
+		var citySelector = {
+
+			init: function(){
+				this._setupListners();
+			},
+
+			_setupListners: function(){
+				$toggle.on('click', citySelector._toggle);
+				$close.on('click', citySelector._toggle);
+			},
+
+			_toggle: function() {
+								
+				if ($cityList.css('display') == 'none') {
+					$cityList.addClass('d-block');
+					$("body").addClass('overflow-hidden');
+				} else {
+					$cityList.removeClass('d-block');
+					$("body").removeClass('overflow-hidden');
+				}
+			}
+		}
+
+		citySelector.init();
+		
+	}());
+
+});
