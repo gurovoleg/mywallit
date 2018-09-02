0d710361b92d88d406d5e50bfc81038356863d3f
maryka <marykaweb@gmail.com>
2018-09-02 23:45:31 +0300

    form order and subscribe

--- src/js/main.js
+++ src/js/main.js
@@ -232,6 +232,24 @@ $(document).ready(function() {
 	$('.footer-subscribe--checkbox-custom')
 		.on( 'focus', function(){ $(this).addClass( 'has-focus' ); })
 		.on( 'blur', function(){ $(this).removeClass( 'has-focus' ); 
-	});		
+	});
+
+	// Открываем всплывающее окно Закать в 1 клик
+	$('a[href="#order-one-click"]').on('click', function(){
+		$('.order-one-click').fadeIn(400);
+	});
+	// Закрытие формы Заказа
+	$('.order-one-click__close-icon').on('click', function(){
+		$('.order-one-click').fadeOut(400);
+	});
+
+	// Открываем всплывающее окно Подписки
+	$('a[href="#order-subscribe"]').on('click', function(){
+		$('.order-subscribe').fadeIn(400);
+	});
+	// Закрытие Подкиски
+	$('.order-subscribe__close-icon').on('click', function(){
+		$('.order-subscribe').fadeOut(400);
+	});
 
 });
