4b0766a1b460e6b52cd17c55a8ea89afed3addf3
gurovoleg <gogotheog@gmail.com>
2018-09-04 18:53:13 +0300

    script update and ui instructions

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -3,11 +3,19 @@ $(document).ready(function() {
 	(function(){
 
 		var $modalWindow = $('.bg-modal-window'),
+			
+			// Обертка для страницы, элементы которой будут перекрываться модальным окном
 			$wrapper = $('.blur-wrapper'),
-			$subscribeOpen = $('#subscribe-button'),
-			$oneClickOpen = $('#one-click-button'),
-			$subscribeClose = $('.order-subscribe__close-icon'),
-			$oneClickClose = $('.order-one-click__close-icon');
+			
+			// Добавить класс к вызывающему элементу (конпки, ссылки, иконки...)
+			$showModal = $(".show-modal-window")
+			// Добавить к класс к закрывающему элементу (конпки, ссылки, иконки...)
+			$closeModal = $(".close-modal-window")
+			
+			//	 !!! Так же необходимо элементу в качестве атрибута data-modal указать класс или id  модального окна, которое будет вызываться !!!!
+			//		Пример:	
+			//		<button class="show-modal-window" data-modal="#modal-window-1">Открыть модальное оккно 1</button>
+			//      <button class="close-modal-window" data-modal="#modal-window-1>Закрыть модальное оккно 1</button>
 
 		var modalWindow = {
 
@@ -17,34 +25,35 @@ $(document).ready(function() {
 
 			_setupListners: function(){
 				
-				$subscribeOpen.on('click', function(){
-					modalWindow._open('#order-subscribe')	
+				$showModal.on('click', function(){
+					mw = $(this).attr('data-modal');
+					modalWindow._open(mw)	
 				});
-				$oneClickOpen.on('click', function(){
-					modalWindow._open('#order-one-click')	
-				});
-				
-				$subscribeClose.on('click', function(){
-					modalWindow._close('#order-subscribe')	
-				});
-				$oneClickClose.on('click', function(){
-					modalWindow._close('#order-one-click')	
+
+				$closeModal.on('click', function(){
+					mw = $(this).attr('data-modal');
+					modalWindow._close(mw);	
 				});
 	
 			},
 
 			_open: function(modalId) {
 											
-				$modalWindow.show();
-				$(modalId).show(200);
-				$wrapper.addClass('filter-blur');
-				$("body").addClass('overflow-hidden');		
+				if (modalId != "" && modalId != undefined) {
+					$modalWindow.show();
+					$(modalId).show(200);
+					$wrapper.addClass('filter-blur');
+					$("body").addClass('overflow-hidden');	
+				}
+	
 			},		
 			
 			_close: function(modalId){
 
-				$modalWindow.hide();
-				$(modalId).hide(200);
+				$(modalId).hide(200,function(){
+					$modalWindow.hide();	
+				});
+				
 				$wrapper.removeClass('filter-blur');
 				$("body").removeClass('overflow-hidden');			
 			
