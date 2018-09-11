dbe5afdfc8fdc04a1adc26c3d9ee067cdf14409a
maryka <marykaweb@gmail.com>
2018-09-12 00:12:12 +0300

    Фикс изображений
    1. png плохо отображается в браузере, для него сделала изображение в jpg
    2. добавлен скрипт изменения изображений при ресайзе страницы
    3. добавлен для больших картинок min-width 100%, чтобы они по центру смотрелись на больших экранах

--- src/js/fix-ie.js
+++ src/js/fix-ie.js
@@ -6,7 +6,6 @@ var ua = window.navigator.userAgent;
 var old_ie = ua.indexOf('MSIE ');
 var new_ie = ua.indexOf('Trident/');
 var w = $(window).width();
-console.log(w);
 
 if ((old_ie > -1) || (new_ie > -1)) {
 	ms_ie = true;
@@ -15,18 +14,35 @@ if ((old_ie > -1) || (new_ie > -1)) {
 if ( ms_ie ) {
 
 	if ( w <= 360 ) {
-		$('.section-banner-1').children().html('<img src="../img/banners/banner_mobile_red.png">');
-		$('.section-banner-2').children().html('<img src="../img/banners/banner_mobile_blue.png">');
+		$('.section-banner-1').children('.container').html('<img src="../img/banners/ie11/banner_mobile_red.jpg">');
+		$('.section-banner-2').children('.container').html('<img src="../img/banners/ie11/banner_mobile_blue.jpg">');
 	} else if ( w <= 784 ) {
-		$('.section-banner-1').children().html('<img src="../img/banners/banner_tablet_red.png">');
-		$('.section-banner-2').children().html('<img src="../img/banners/banner_tablet_blue.png">');
+		$('.section-banner-1').children('.container').html('<img src="../img/banners/ie11/banner_tablet_red.jpg">');
+		$('.section-banner-2').children('.container').html('<img src="../img/banners/ie11/banner_tablet_blue.jpg">');
 	} else if ( w <= 1152 ) {
-		$('.section-banner-1').children().html('<img src="../img/banners/banner_desktop_red.png">');
-		$('.section-banner-2').children().html('<img src="../img/banners/banner_desktop_blue.png">');
+		$('.section-banner-1').children('.container').html('<img src="../img/banners/ie11/banner_desktop_red.jpg">');
+		$('.section-banner-2').children('.container').html('<img src="../img/banners/ie11/banner_desktop_blue.jpg">');
 	} else {
-		$('.section-banner-1').children().html('<img src="../img/banners/banner_desktopxl_red.png">');
-		$('.section-banner-2').children().html('<img src="../img/banners/banner_desktopxl_blue.png">');
+		$('.section-banner-1').children('.container').html('<img src="../img/banners/ie11/banner_desktopxl_red.jpg" style="min-width: 100%;">');
+		$('.section-banner-2').children('.container').html('<img src="../img/banners/ie11/banner_desktopxl_blue.jpg" style="min-width: 100%;">');
 	}
+
+	$( window ).resize(function() {
+		var w = $(this).width();
+		if ( w <= 360 ) {
+			$('.section-banner-1').children('.container').html('<img src="../img/banners/ie11/banner_mobile_red.jpg">');
+			$('.section-banner-2').children('.container').html('<img src="../img/banners/ie11/banner_mobile_blue.jpg">');
+		} else if ( w <= 784 ) {
+			$('.section-banner-1').children('.container').html('<img src="../img/banners/ie11/banner_tablet_red.jpg">');
+			$('.section-banner-2').children('.container').html('<img src="../img/banners/ie11/banner_tablet_blue.jpg">');
+		} else if ( w <= 1152 ) {
+			$('.section-banner-1').children('.container').html('<img src="../img/banners/ie11/banner_desktop_red.jpg">');
+			$('.section-banner-2').children('.container').html('<img src="../img/banners/ie11/banner_desktop_blue.jpg">');
+		} else {
+			$('.section-banner-1').children('.container').html('<img src="../img/banners/ie11/banner_desktopxl_red.jpg" style="min-width: 100%;">');
+			$('.section-banner-2').children('.container').html('<img src="../img/banners/ie11/banner_desktopxl_blue.jpg" style="min-width: 100%;">');
+		}
+	});
 
 }
 // END Фикс адаптивность банеров для IE 11
