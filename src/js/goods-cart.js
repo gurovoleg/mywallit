86f97977bccbb76d5419c31d1c70ec124253ef97
maryka <marykaweb@gmail.com>
2018-09-23 20:55:41 +0300

    Возврат overflow auto

--- src/js/goods-cart.js
+++ src/js/goods-cart.js
@@ -60,6 +60,7 @@ $(document).mouseup(function (e){
 					$('.goods-cart__page-bg').css('display', 'none');
 					$('.blur-wrapper').removeClass('filter-blur');
 				}, 600);
+			$('body').css('overflow', 'auto');
 			}
 	}
 });
