3d1f3961d2d3c07c7fd236df0cb407cabfdc7e1f
gurovoleg <gogotheog@gmail.com>
2018-09-27 12:56:23 +0300

    script update and css update

--- src/js/filter-menu.js
+++ src/js/filter-menu.js
@@ -3,37 +3,42 @@ $(document).ready(function() {
 	var currentWindowSize = $(window).width();
 
 	// Показать/закрыть категории фильтра
-	$('.filter-menu-l1 li').on('click', function(e){
+	$('.filter-menu-block__title').on('click', function(e){
+		console.log('title click');
+		console.log(this);
+		console.log(e.target);
+		
 		if (this == e.target) {
+			console.log('inside');
 			if ($(window).width() <= tabletWidth){
-				$(this).find('ul').slideToggle();
-				$(this).children(':first').toggleClass('is-opened');
+				$(this).parent().children('.filter-submenu-block').slideToggle(150);
+				$(this).children().toggleClass('is-opened');
 			}
 		}	
 	});
 
 	// Показать/закрыть категории фильтра по иконке
 	$('.filter-icon-close').on('click', function(e){
-		$(this).parent().find('ul').slideToggle();
+		$(this).parent().next().slideToggle(150);
 		$(this).toggleClass('is-opened');
 	});
 
 	// Показываем все внутренние меню при переходе на десктоп
 	// а так же при обратном переходе свррачиваем меню и возвращаем крестик
-	$(window).resize(function(){
-		var w = $(window).width();
+	// $(window).resize(function(){
+	// 	var w = $(window).width();
 	
-		if (w >= tabletWidth)  {
-		    $('.filter-menu-l2').show();	
-		} else {
-			if (currentWindowSize >= tabletWidth) {
-				$('.filter-menu-l2').hide();
-				$('.filter-icon-close').removeClass('is-opened');
-			}
-		}
-		currentWindowSize = w;
+	// 	if (w >= tabletWidth)  {
+	// 	    $('.filter-menu-l2').show();	
+	// 	} else {
+	// 		if (currentWindowSize >= tabletWidth) {
+	// 			$('.filter-menu-l2').hide();
+	// 			$('.filter-icon-close').removeClass('is-opened');
+	// 		}
+	// 	}
+	// 	currentWindowSize = w; 
 
-	}); 
+	// }); 
 
 	// Очищием форму
 	$('.filter__clean-button a').on('click', function(e){
@@ -41,5 +46,4 @@ $(document).ready(function() {
 		$(".form-filter")[0].reset();	
 	});
 	
-
 });
