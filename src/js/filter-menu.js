b7a86d2c9ff061dab95d070b1dfd2b12537a8a20
gurovoleg <gogotheog@gmail.com>
2018-09-27 14:53:32 +0300

    script fix

--- src/js/filter-menu.js
+++ src/js/filter-menu.js
@@ -4,12 +4,7 @@ $(document).ready(function() {
 
 	// Показать/закрыть категории фильтра
 	$('.filter-menu-block__title').on('click', function(e){
-		console.log('title click');
-		console.log(this);
-		console.log(e.target);
-		
 		if (this == e.target) {
-			console.log('inside');
 			if ($(window).width() <= tabletWidth){
 				$(this).parent().children('.filter-submenu-block').slideToggle(150);
 				$(this).children().toggleClass('is-opened');
@@ -18,32 +13,32 @@ $(document).ready(function() {
 	});
 
 	// Показать/закрыть категории фильтра по иконке
-	$('.filter-icon-close').on('click', function(e){
+	$('.filter-item-toggle').on('click', function(e){
 		$(this).parent().next().slideToggle(150);
 		$(this).toggleClass('is-opened');
 	});
-
-	// Показываем все внутренние меню при переходе на десктоп
-	// а так же при обратном переходе свррачиваем меню и возвращаем крестик
-	// $(window).resize(function(){
-	// 	var w = $(window).width();
-	
-	// 	if (w >= tabletWidth)  {
-	// 	    $('.filter-menu-l2').show();	
-	// 	} else {
-	// 		if (currentWindowSize >= tabletWidth) {
-	// 			$('.filter-menu-l2').hide();
-	// 			$('.filter-icon-close').removeClass('is-opened');
-	// 		}
-	// 	}
-	// 	currentWindowSize = w; 
-
-	// }); 
 
 	// Очищием форму
 	$('.filter__clean-button a').on('click', function(e){
 		e.preventDefault();
 		$(".form-filter")[0].reset();	
 	});
+
+	// Показываем все внутренние меню при переходе на десктоп
+	// а так же при обратном переходе свррачиваем меню и возвращаем крестик
+	$(window).resize(function(){
+		var w = $(window).outerWidth();
+
+		if (w >= tabletWidth)  {
+		    // console.log(w);
+		    $('.catalog-filter').removeClass('catalog-filter--show')
+		}
+		// } else if (currentWindowSize >= tabletWidth) {
+		// 	$('.filter-submenu-block').hide();
+		// 	$('.filter-item-toggle').removeClass('is-opened');
+		// }
+		// currentWindowSize = w; 
+	}); 
+
 	
 });
